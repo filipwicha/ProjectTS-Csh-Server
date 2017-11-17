7d869cd85f2be8e77efd41d33c6bab822b5e678e
filipwicha <33604199+filipwicha@users.noreply.github.com>
2017-11-17 12:20:30 +0100

    server without changes

--- ProjectTS Csh Server/Program.cs
+++ ProjectTS Csh Server/Program.cs
@@ -1,15 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//
+/*   Server Program    */
 
-namespace ProjectTS_Csh_Server
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+//REMEMBER TO WRITE IP ADRESS DOWN THERE
+
+public class serv
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        try
         {
+            IPAddress ipAd = IPAddress.Parse("172.21.5.99");
+            // use local m/c IP address, and 
+            // use the same in the client
+
+            /* Initializes the Listener */
+            TcpListener myList = new TcpListener(ipAd, 8001);
+
+            /* Start Listeneting at the specified port */
+            myList.Start();
+
+            Console.WriteLine("The server is running at port 8001...");
+            Console.WriteLine("The local End point is  :" +
+                              myList.LocalEndpoint);
+            Console.WriteLine("Waiting for a connection.....");
+
+            Socket s = myList.AcceptSocket();
+            Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+            byte[] b = new byte[100];
+            int k = s.Receive(b);
+            Console.WriteLine("Recieved...");
+            for (int i = 0; i < k; i++)
+                Console.Write(Convert.ToChar(b[i]));
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            s.Send(asen.GetBytes("The string was recieved by the server."));
+            Console.WriteLine("\nSent Acknowledgement");
+            /* clean up */
+            s.Close();
+            myList.Stop();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error..... " + e.StackTrace);
         }
     }
+
 }
